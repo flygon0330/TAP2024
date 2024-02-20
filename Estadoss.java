bcae7e90d14d8440d85688eddf21759099dfda2c
Jose <jalfredoef18@gmail.com>
2024-02-20 16:45:25 -0600

    Estados

---
+++ Estadoss.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Estadoss {
+    private JPanel panel1;
+    private JPanel panel2;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton OKButton;
+    private JButton cancelarButton;
+
+
+    public Estadoss() {
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
