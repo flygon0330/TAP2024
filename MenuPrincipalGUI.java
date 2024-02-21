9b4bba3ebc7fb74335f7adc125d7a35bffbf868f
gustavo <l21020390@veracruz.tecnm.mx>
2024-02-21 13:24:28 -0600

    Cambios en la interfaz de los menus

---
+++ MenuPrincipalGUI.java
@@ -0,0 +1,14 @@
+package Interfaces;
+
+import javax.swing.*;
+
+public class MenuPrincipalGUI {
+    private JPanel panel1;
+    private JButton clientesButton;
+    private JButton productosButton;
+    private JButton proveedoresButton;
+    private JButton ciudadesButton;
+    private JButton estadosButton;
+    private JButton listasButton;
+    private JButton salirButton;
+}
---
+++ ProveedoresGUI.java
@@ -0,0 +1,24 @@
+package Interfaces;
+
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+
+public class ProveedoresGUI {
+    private JPanel panel1;
+    private JTextField txtNombre;
+    private JTextField txtClave;
+    private JComboBox cbMp;
+    private JButton cancelarButton;
+    private JButton guardarButton;
+    private JButton regresarButton;
+    private JTextField textField1;
+
+    public ProveedoresGUI() {
+        cbMp.addComponentListener(new ComponentAdapter() {
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
